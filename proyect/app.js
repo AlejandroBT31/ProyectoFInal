8834377898e034465cfcde6df9a45588e52abd9d
AlejandroBT31 <albato31@gmail.com>
2021-12-09 18:13:13 -0600

    Feat: add Sentry

--- proyect/app.js
+++ proyect/app.js
@@ -31,10 +31,47 @@ module.exports = app;
 
 const http = require('http');
 
-const port = 3000;
+const port = 5000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hola Mundo');
+  res.end('Hola Mundo - Docker');
 });
+
+//Sentry
+
+const Sentry = require("@sentry/node");
+// or use es6 import statements
+// import * as Sentry from '@sentry/node';
+
+const Tracing = require("@sentry/tracing");
+// or use es6 import statements
+// import * as Tracing from '@sentry/tracing';
+
+Sentry.init({
+  dsn: "https://b18c98d89fe2463492810ccf48e59c38@o1059781.ingest.sentry.io/6099457",
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
+});
+
+const transaction = Sentry.startTransaction({
+  op: "test",
+  name: "My First Test Transaction",
+});
+
+setTimeout(() => {
+  try {
+    foo();
+  } catch (e) {
+    Sentry.captureException(e);
+  } finally {
+    transaction.finish();
+  }
+}, 99);
+function foo() {
+  throw new Error('Function not implemented.');
+}
